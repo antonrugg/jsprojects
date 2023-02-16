022238e2f705c0cd5b7baebdc60024e5b202b72e
Antonio Ruggiero <antonrugg@gmail.com>
2023-02-16 19:47:10 +0100

    feat: init commit for timerjs project, create Timer class, added basic methods for start, pause the timer anda decrement value every second.

---
+++ timerjs/index.js
@@ -0,0 +1,38 @@
+class Timer{
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+
+    }
+ 
+    start = () => {
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.intervalId);
+    }
+
+    tick = () => {
+        const timeRemaining = parseFloat(this.durationInput.value);
+        this.durationInput.value = timeRemaining - 1;
+
+   }
+
+   
+}
+
+const durationinput = document.querySelector('#duration');
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationinput, startButton, pauseButton);
+
+
+
+
